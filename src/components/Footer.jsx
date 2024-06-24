0638445f42e985f7cf68c86b3f7a225a42198716
Rorisang Putu <rorisangputu@gmail.com>
2024-06-25 01:02:58 +0200

    added footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,35 @@
-
+import { FaTwitter, FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div className='h-[10vh] border-t flex justify-center items-center text-white text-sm sm:text-medium md:text-lg'>
-            <p>Copyright &copy; 2024, CoinVue. All rights reserved.</p>
+        <div className='h-[40vh] flex justify-center items-center text-white text-sm sm:text-medium md:text-lg'>
+            <div className='flex flex-col items-center'>
+                <div className='flex justify-center mb-4'>
+                    <a href="#" className='mr-4'>
+                        <FaTwitter size={20} />
+                    </a>
+                    <a href="#" className='mr-4'>
+                        <FaFacebook size={20} />
+                    </a>
+                    <a href="#" className='mr-4'>
+                        <FaInstagram size={20} />
+                    </a>
+                    <a href="#">
+                        <FaYoutube size={20} />
+                    </a>
+                </div>
+                <div className='flex justify-center pb-3'>
+                    <a href="#" className='mr-4 text-white hover:text-gray-200'>
+                        Privacy Policy
+                    </a>
+                    <a href="#" className='mr-4 text-white hover:text-gray-200'>
+                        Terms of Use
+                    </a>
+                </div>
+                <p>Copyright &copy; 2024, CoinVue. All rights reserved.</p>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
