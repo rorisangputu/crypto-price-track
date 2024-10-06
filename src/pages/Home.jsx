eb1128471e8ff44f013cdc4818c8dd63add61ac0
Rorisang Putu <rorisangputu@gmail.com>
2024-10-06 02:39:11 +0200

    making more changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -52,84 +52,87 @@ const Home = () => {
 
 
     return (
-        <div className="w-[90%] mx-auto my-5">
-            <Hero setDisplayCoin={setDisplayCoin} allCoin={allCoin} />
-            <h1 id="market-updates"
-                className="text-white text-4xl md:text-7xl lg:text-9xl py-7 
+        <div className="w-full">
+            <div className="w-[90%] mx-auto my-5">
+                <Hero setDisplayCoin={setDisplayCoin} allCoin={allCoin} />
+                <h1 id="market-updates"
+                    className="text-white text-4xl md:text-7xl lg:text-9xl py-7 
                   text-center font-bold"
-            >
-                Market Updates
-            </h1>
-            <form
-                onSubmit={searchHandler}
-                className="my-5 bg-white h-11 md:h-12 flex px-2 
-                justify-between items-center rounded-lg">
-                <input
-                    onChange={inputHandler} required value={input}
-                    className="w-60 md:w-80 lg:w-[80%] bg-transparent h-8 text-black md:h-10 outline-none rounded-lg"
-                    type="text"
-                    placeholder="Search crypto.."
-                    list="coinlist"
-                />
-
-                <datalist id="coinlist">
-                    {allCoin.map((item, index) => (
-                        <option className="bg-white" key={index} value={item.name} />
-                    ))}
-                </datalist>
-
-                <button className="px-4 flex justify-center items-center text-white bg-purple-900 w-16 
-                        md:w-20 rounded-lg h-8 md:h-10" type="submit"
                 >
-                    Search
-                </button>
-            </form>
-            {/* sm:max-w-[600px] md:max-w-[700px] lg:max-w-[900px] */}
-            <div className="flex justify-between text-white text-medium mb-2">
-                <p className="ml-1">Currency: {currency.symbol}</p>
-                <select onChange={currencyHandler} className='bg-transparent w-[70px] rounded-md font-medium 
+                    Market Updates
+                </h1>
+                <form
+                    onSubmit={searchHandler}
+                    className="my-5 lg:w-[80%] mx-auto bg-white h-11 md:h-12 flex px-2 
+                justify-between items-center rounded-lg">
+                    <input
+                        onChange={inputHandler} required value={input}
+                        className="w-60 md:w-80 lg:w-[80%] bg-transparent h-8 text-black md:h-10 outline-none rounded-lg"
+                        type="text"
+                        placeholder="Search crypto.."
+                        list="coinlist"
+                    />
+
+                    <datalist id="coinlist">
+                        {allCoin.map((item, index) => (
+                            <option className="bg-white" key={index} value={item.name} />
+                        ))}
+                    </datalist>
+
+                    <button className="px-4 flex justify-center items-center text-white bg-purple-900 w-16 
+                        md:w-20 rounded-lg h-8 md:h-10" type="submit"
+                    >
+                        Search
+                    </button>
+                </form>
+                {/* sm:max-w-[600px] md:max-w-[700px] lg:max-w-[900px] */}
+                <div className="flex justify-between text-white text-medium mb-2 lg:w-[80%] mx-auto">
+                    <p className="ml-1">Currency: {currency.symbol}</p>
+                    <select onChange={currencyHandler} className='bg-transparent w-[70px] rounded-md font-medium 
                 '>
-                    <option value="usd">USD</option>
-                    <option value="eur">EUR</option>
-                    <option value="zar">ZAR</option>
-                </select>
-            </div>
-            {/* md:max-w-[700px] lg:max-w-[900px] mx-auto */}
-            <div className="shadow-xl rounded-lg mb-10">
-                <div className="grid grid-cols-[0.2fr,2fr,1fr,1fr,1.5fr] p-4 rounded-t-xl  items-center text-white bg-gradient-to-r from-[#2600fc] to-[#ff00ea] ">
-                    <p>#</p>
-                    <p className="font-semibold">Coins</p>
-                    <p className="font-semibold">Price</p>
-                    <p className="font-semibold">24h %</p>
-                    <p className="font-semibold">Market Cap</p>
+                        <option value="usd">USD</option>
+                        <option value="eur">EUR</option>
+                        <option value="zar">ZAR</option>
+                    </select>
                 </div>
-                {displayCoin.slice(0, 10).map((item, index) => (
-                    <Link to={`/coin/${item.id}`} key={index} className="border-b grid grid-cols-[0.2fr,2fr,1fr,1fr,1.5fr] p-4 flex items-center text-white  text-sm md:text-lg">
-                        <p>{item.market_cap_rank}</p>
-                        <div style={{
-                            whiteSpace: 'nowrap',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
+                {/* md:max-w-[700px] lg:max-w-[900px] mx-auto */}
+                <div className="shadow-xl rounded-lg mb-10 lg:w-[80%] mx-auto">
+                    <div className="grid grid-cols-[0.2fr,2fr,1fr,1fr,1.5fr] p-4 rounded-t-xl  items-center text-white bg-gradient-to-r from-[#2600fc] to-[#ff00ea] ">
+                        <p>#</p>
+                        <p className="font-semibold">Coins</p>
+                        <p className="font-semibold">Price</p>
+                        <p className="font-semibold">24h %</p>
+                        <p className="font-semibold">Market Cap</p>
+                    </div>
+                    {displayCoin.slice(0, 10).map((item, index) => (
+                        <Link to={`/coin/${item.id}`} key={index} className="border-b grid grid-cols-[0.2fr,2fr,1fr,1fr,1.5fr] p-4 flex items-center text-white  text-sm md:text-lg">
+                            <p>{item.market_cap_rank}</p>
+                            <div style={{
+                                whiteSpace: 'nowrap',
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
 
-                        }}
-                            className="flex w-[100px] md:w-[200px] lg:w-full items-center gap-1 sm:gap-3 md:gap-5">
-                            <img className="h-4 sm:h-6 lg:h-8 ml-1 sm:ml-2 md:ml-3" src={item.image} alt="" />
-                            <p className="ml-2">{item.name}</p>
-                        </div>
-                        <p>{item.current_price.toLocaleString()}</p>
-                        <p className={item.price_change_percentage_24h > 0 ? "text-green-600" : "text-red-600"}>{Math.floor(item.price_change_percentage_24h * 100) / 100}</p>
-                        <p style={{
-                            whiteSpace: 'nowrap',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                        }}>
-                            {item.market_cap ? Number(item.market_cap).toLocaleString() : 'N/A'}
-                        </p>
+                            }}
+                                className="flex w-[100px] md:w-[200px] lg:w-full items-center gap-1 sm:gap-3 md:gap-5">
+                                <img className="h-4 sm:h-6 lg:h-8 ml-1 sm:ml-2 md:ml-3" src={item.image} alt="" />
+                                <p className="ml-2">{item.name}</p>
+                            </div>
+                            <p>{item.current_price.toLocaleString()}</p>
+                            <p className={item.price_change_percentage_24h > 0 ? "text-green-600" : "text-red-600"}>{Math.floor(item.price_change_percentage_24h * 100) / 100}</p>
+                            <p style={{
+                                whiteSpace: 'nowrap',
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                            }}>
+                                {item.market_cap ? Number(item.market_cap).toLocaleString() : 'N/A'}
+                            </p>
 
-                    </Link>
-                ))}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
+
     )
 }
 
