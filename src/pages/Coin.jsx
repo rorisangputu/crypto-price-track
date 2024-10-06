fa5d02886bb7056e6e483edc904adb2fc5c81fcc
Rorisang Putu <rorisangputu@gmail.com>
2024-10-06 03:02:43 +0200

    fixing issues

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -60,27 +60,33 @@ const Coin = () => {
         );
     }
     return (
-        <div className="flex flex-col items-center justify-center  md:h-screen">
-            <div className=" text-white container gap-x-3 lg:w-[1200px] flex flex-col lg:flex-row items-center mx-auto">
-                <div className="mx-auto text-center flex flex-col items-center">
-                    <img src={coinData.image?.large} alt={coinData.name} className="object-contain" />
-                    <h1 className="text-5xl font-semibold my-6">{coinData.name}</h1>
-                    <h3 className="text-2xl font-medium">Rank: {coinData.market_cap_rank}</h3>
+        <div className="w-full py-10">
+            <div className="w-[90%] mx-auto">
+                <div className="flex flex-col items-center justify-center">
+                    <div className=" text-white gap-x-3 flex flex-col lg:flex-row items-center">
+                        <div className="mx-auto lg:w-2/5 text-center flex flex-col items-center">
+                            <div className="h-24 w-24">
+                                <img src={coinData.image?.large} alt={coinData.name} className="object-contain" />
+                            </div>
+                            <h1 className="text-5xl font-semibold my-6">{coinData.name}</h1>
+                            <h3 className="text-2xl font-medium">Rank: {coinData.market_cap_rank}</h3>
 
-                </div>
-                <hr className="w-[400px] my-5 lg:rotate-90 lg:w-[400px]" />
-                <div className="flex flex-col">
-                    <div className="text-center px-5 gap-x-5 lg:flex">
-                        <p className="lg:text-xl">Symbol: {coinData.symbol}</p>
-                        <p className="lg:text-xl">Price: <span className="text-green-500">${coinData.market_data?.current_price[currency.name].toLocaleString()}</span></p>
+                        </div>
+                        <hr className=" lg:my-5 lg:rotate-90 " />
+                        <div className="flex flex-col lg:w-3/5 justify-center items-center">
+                            <div className="text-center px-5 gap-x-5 lg:flex">
+                                <p className="lg:text-xl">Symbol: {coinData.symbol}</p>
+                                <p className="lg:text-xl">Price: <span className="text-green-500">${coinData.market_data?.current_price[currency.name].toLocaleString()}</span></p>
+                            </div>
+                            <div className="w-[90%] py-5">
+                                <p className="max-w-sm sm:max-w-xl xl:max-w-5xl lg:h-[500px] overflow-hidden lg:text-lg">{coinData.description ? coinData.description.en : ""}</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="px-5 py-5">
-                        <p className=" lg:text-lg">{coinData.description ? coinData.description.en : ""}</p>
+                    <div className="">
+                        <h3 className="text-white rounded-xl p-2 my-2 bg-gradient-to-r from-[#2600fc] to-[#ff00ea]"><a href="/">Back Home</a></h3>
                     </div>
                 </div>
-            </div>
-            <div>
-                <h3 className="text-white rounded-xl p-2 my-2 bg-gradient-to-r from-[#2600fc] to-[#ff00ea]"><a href="/">Back Home</a></h3>
             </div>
         </div>
     )
